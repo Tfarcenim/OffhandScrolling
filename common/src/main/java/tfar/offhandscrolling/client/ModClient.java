b0f9850074ebfa9c082d010596fccaf727eb41e0
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2024-08-23 10:49:04 -0400

    changes

--- common/src/main/java/tfar/offhandscrolling/client/ModClient.java
+++ common/src/main/java/tfar/offhandscrolling/client/ModClient.java
@@ -2,11 +2,10 @@ package tfar.offhandscrolling.client;
 
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ClickType;
+import net.minecraft.world.inventory.InventoryMenu;
 import net.minecraft.world.item.ItemStack;
 import org.apache.commons.lang3.ArrayUtils;
 import org.lwjgl.glfw.GLFW;
@@ -14,11 +13,14 @@ import org.lwjgl.glfw.GLFW;
 public class ModClient {
 
     public static final KeyMapping MODIFIER = new KeyMapping("modifier", GLFW.GLFW_KEY_LEFT_ALT,"Offhand Category");
+    public static final KeyMapping SWAP_ABOVE = new KeyMapping("swap_above", GLFW.GLFW_KEY_LEFT_ALT,"Offhand Category");
 
-    static int offhandSelected;
+
+    static boolean state;
 
     public static void keyBinds() {
         registerKeyBind(MODIFIER);
+        registerKeyBind(SWAP_ABOVE);
     }
 
     public static void registerKeyBind(KeyMapping keyMapping) {
@@ -26,46 +28,27 @@ public class ModClient {
     }
 
     public static void handleKeybinds(Minecraft minecraft) {
-        if (areAnyHotbarKeysDown(minecraft) && MODIFIER.consumeClick()) {
-            for(int i = 0; i < 9; ++i) {
-                if (minecraft.options.keyHotbarSlots[i].consumeClick()) {
-                    if (minecraft.player.isSpectator()) {
-                 //       minecraft.gui.getSpectatorGui().onHotbarSelected(i);
-                    } else {
-                        trySwapOffhand(i);
-                    }
-                }
-            }
+        if (SWAP_ABOVE.consumeClick()) {
+            state = !state;
+            trySwapOffhand2(state);
         }
     }
 
-    public static boolean trySwapOffhand(int newSlot) {
-        int oldSlot = offhandSelected;
-        if (oldSlot != newSlot) {
-
+    public static boolean trySwapOffhand2(boolean up) {
             Minecraft minecraft = Minecraft.getInstance();
             Player player = minecraft.player;
             if (player != null) {
-                AbstractContainerMenu menu = player.inventoryMenu;
+                InventoryMenu menu = player.inventoryMenu;
 
                 ItemStack offhandStack = player.getInventory().offhand.get(0);
+                //swap from 0 to 1 for example
 
-                int oldInvSlot = oldSlot + 27;
-
-                minecraft.gameMode.handleInventoryMouseClick(menu.containerId, oldInvSlot, 40, ClickType.SWAP, player);
-                minecraft.gameMode.handleInventoryMouseClick(menu.containerId, newSlot + 27, 40, ClickType.SWAP, player);
-                offhandSelected = newSlot;
+                if (up && offhandStack.isEmpty()) {
+                    minecraft.gameMode.handleInventoryMouseClick(menu.containerId, player.getInventory().selected + 27, 40, ClickType.SWAP, player);
+                } else if (!up && !offhandStack.isEmpty()) {
+                    minecraft.gameMode.handleInventoryMouseClick(menu.containerId, player.getInventory().selected + 27, 40, ClickType.SWAP, player);
+                }
                 return true;
-            }
-        }
-        return false;
-    }
-
-    public static boolean areAnyHotbarKeysDown(Minecraft minecraft) {
-        for(int i = 0; i < 9; ++i) {
-            if (minecraft.options.keyHotbarSlots[i].isDown()) {
-                return true;
-            }
         }
         return false;
     }
@@ -73,15 +56,7 @@ public class ModClient {
     public static boolean handleScroll(Inventory inventory, double direction) {
         if (MODIFIER.isDown()) {
             int i = (int)Math.signum(direction);
-            int newOffhandSelected = offhandSelected;
-            for(newOffhandSelected -= i; newOffhandSelected < 0; newOffhandSelected += 9) {
-            }
-
-            while(newOffhandSelected >= 9) {
-                newOffhandSelected -= 9;
-            }
-
-            return trySwapOffhand(newOffhandSelected);
+            return trySwapOffhand2(i > 0);
         }
         return false;
     }
